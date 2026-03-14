ee03842d175f01f5af09fb4d7f4c84fa2fddef62
Luhlolwethu Mncwabe <263221050+LuhlolwethuMncwabe@users.noreply.github.com>
2026-03-14 22:13:06 +0200

    Added the Venue Model.

---
+++ EventEase/Models/Venue.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Models
+{
+    public class Venue
+    {
+        public int VenueID { get; set; }
+
+        [Required]
+        [Display(Name = "Venue Name")]
+        public string NameOfVenue { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string VenueAddress { get; set; }
+
+        [Required]
+        public int Capacity { get; set; }
+
+        [Required]
+        [Display(Name = "Location")]
+        public string VenueLocation { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; }
+    }
+}
