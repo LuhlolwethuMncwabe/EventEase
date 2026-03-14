9a5d03347fe572302f81849e9629aaaa9a7b10f8
Luhlolwethu Mncwabe <263221050+LuhlolwethuMncwabe@users.noreply.github.com>
2026-03-14 23:03:55 +0200

    Setup database connection.

---
+++ EventEase/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EventEase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventEase.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
