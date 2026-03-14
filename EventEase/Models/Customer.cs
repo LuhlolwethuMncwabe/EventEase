eba26c5cfd876f4d347ce7b6c06537c689341bd9
Luhlolwethu Mncwabe <263221050+LuhlolwethuMncwabe@users.noreply.github.com>
2026-03-14 22:25:59 +0200

    Added the Customer model.

---
+++ EventEase/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
