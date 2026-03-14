3e4021e97c7b0292faa166d6a5abd47151808b69
Luhlolwethu Mncwabe <263221050+LuhlolwethuMncwabe@users.noreply.github.com>
2026-03-14 22:28:17 +0200

    Adding the Booking model.

---
+++ EventEase/Models/Booking.cs
@@ -0,0 +1,36 @@
+﻿using EventEase.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Models
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        [Required]
+        [Display(Name = "Total Amount")]
+        [DataType(DataType.Currency)]
+        public decimal TotalAmount { get; set; }
+
+        [Required]
+        [Display(Name = "Number of Seats")]
+        public int NumberOfSeats { get; set; }
+
+        [Display(Name = "Booked At")]
+        public DateTime BookedAt { get; set; } = DateTime.Now;
+
+        // Foreign Keys
+        public int EventID { get; set; }
+        public int CustomerID { get; set; }
+
+        // Navigation Properties
+        [Display(Name = "Event")]
+        public Event Event { get; set; }
+
+        [Display(Name = "Customer")]
+        public Customer Customer { get; set; }
+    }
+}
